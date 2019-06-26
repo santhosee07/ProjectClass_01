a9b12b1117b22f8a7224662fca4479031f79b7be
santhosee07 <sanshi713@gmail.com>
2019-06-26 22:58:23 +0530

    maven added to git

---
+++ src/main/java/org/maven/MavenSelenium/App.java
@@ -0,0 +1,13 @@
+package org.maven.MavenSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/maven/MavenSelenium/DataDriven.java
@@ -0,0 +1,54 @@
+package org.maven.MavenSelenium;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+public class DataDriven {
+	
+	public static void main(String[] args) throws IOException {
+		File loc = new File("C:\\Users\\Navin\\eclipse-workspace\\MavenSelenium\\excel\\datas.xlsx");
+		FileInputStream f=new FileInputStream(loc);
+		Workbook w=new XSSFWorkbook(f);
+		Sheet s = w.getSheet("Sheet1");
+		int rows= s.getPhysicalNumberOfRows();
+		System.out.println(rows);
+		Row row = s.getRow(3);
+		Cell cell = row.getCell(2);
+		System.out.println(cell);
+		int type= cell.getCellType();
+		System.out.println(type);
+		if(type==0) {
+			if(DateUtil.isCellDateFormatted(cell)) {
+				Date d = cell.getDateCellValue();
+				SimpleDateFormat sim = new SimpleDateFormat("mm/dd/yyyy");
+			    String format=sim.format(d);
+				System.out.println(format);
+			}
+			else {
+				double num = cell.getNumericCellValue();
+				long l = (long)num;
+				String number = String.valueOf(1);
+				System.out.println(number);
+			}
+		}
+				
+		}
+		
+	
+}
+	
+	
+
+
